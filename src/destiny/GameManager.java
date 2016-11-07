716d17cd0564f99a74793430eac197bda6c3c944
Baudouin <baudouin.landais@isen-lille.fr>
2016-11-07 13:36:52 +0100

    Début game manager

--- src/destiny/GameManager.java
+++ src/destiny/GameManager.java
@@ -1,14 +1,45 @@
 package destiny;
 
+import destiny.mover.Player;
+
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * Classe qui gère la dynamique du jeu
  */
 public class GameManager {
 
+    private static Player player;
+
     public static void main(String[] args) {
         System.out.println("Bienvenue dans Destiny");
+        askPlayerName();
+        //  player = new Player(askPlayerName(), 100, );
+    }
+
+    private static String askPlayerName() {
+        String name = "";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Quel est votre nom ?");
+        name = scanner.nextLine();
+        while(name.isEmpty()) {
+            System.out.println("Le nom saisi est invalide. Quel est votre nom ?");
+            name = scanner.nextLine();
+        }
+        return name;
+    }
+
+    public static void manageInventory() {
+
+    }
+
+    public static void manageActions() {
+
+    }
+
+    public static void generateLevel() {
+
     }
 
 }
---
+++ test/destiny/GameManagerTest.java
@@ -0,0 +1,28 @@
+package destiny;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Baudouin on 06/11/2016.
+ */
+public class GameManagerTest {
+
+    @Test
+    public void manageInventory() throws Exception {
+
+    }
+
+    @Test
+    public void manageActions() throws Exception {
+
+    }
+
+    @Test
+    public void generateLevel() throws Exception {
+
+    }
+
+}
